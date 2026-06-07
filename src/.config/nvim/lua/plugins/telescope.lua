cadd8a96e06eb6ea972047e05b7306580b856403
kkgwshm <kikugawa.s.shukatsu@gmail.com>
2026-06-07 22:25:16 +0900

    feat(nvim): search hidden files in telescope while excluding .git
    Enable hidden file search in find_files and live_grep so dotfiles are
    discoverable, and exclude the .git directory from results to avoid noise.

--- src/.config/nvim/lua/plugins/telescope.lua
+++ src/.config/nvim/lua/plugins/telescope.lua
@@ -3,7 +3,22 @@ return {
     "nvim-telescope/telescope.nvim", 
     opts = {},
     config = function()
-      require('telescope').setup({})
+      require('telescope').setup({
+        defaults = {
+          -- .git ディレクトリ配下は検索対象から除外する
+          file_ignore_patterns = { '^.git/' },
+        },
+        pickers = {
+          find_files = {
+            -- ドットファイルなどの隠しファイルも検索対象に含める
+            hidden = true,
+          },
+          live_grep = {
+            -- 隠しファイルを検索しつつ .git 配下は除外する
+            additional_args = { '--hidden', '--glob=!**/.git/*' },
+          },
+        },
+      })
 
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
